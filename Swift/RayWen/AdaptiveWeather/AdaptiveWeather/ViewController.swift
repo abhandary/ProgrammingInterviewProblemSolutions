2c58a296f4e3baeab5b7e244c438e02449a9f065
Akshay Bhandary <akshayb@yahoo-inc.com>
2016-06-26 16:07:55 -0700

    added Adpative Layout

---
+++ Swift/RayWen/AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Akshay Bhandary on 6/25/16.
+//  Copyright © 2016 Akshay Bhandary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
